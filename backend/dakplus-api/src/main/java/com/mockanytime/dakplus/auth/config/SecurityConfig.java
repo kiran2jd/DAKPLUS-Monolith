f3ebbdbb575dbbc2dc65262f599f89af13f40e2d
kkumar <kkumar@1800flowers.com>
2026-03-05 03:23:36 +0530

    security config issue fix

--- backend/dakplus-api/src/main/java/com/mockanytime/dakplus/auth/config/SecurityConfig.java
+++ backend/dakplus-api/src/main/java/com/mockanytime/dakplus/auth/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.beans.factory.annotation.Value;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
